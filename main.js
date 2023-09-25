1bdaccf6968e0222e50f3ad289506281ee461c60
Sergio Aguilar <ssaguilara3@gmail.com>
2023-09-25 12:27:10 -0500

    Se actualiza css y se agrega nuevo proyecto realizado con react

--- main.js
+++ main.js
@@ -1,120 +1,104 @@
-const iconoNav = document.querySelector(".icono-nav");
+// Funciones para manejar el menú responsive
 const menu = document.querySelector(".menu");
+const iconoNav = document.querySelector(".icono-nav");
 const iconoClose = document.querySelector(".menu div");
-const linkMenu = [...document.querySelectorAll(".menu a")]
+const linkMenu = document.querySelectorAll(".menu a");
 
-iconoNav.addEventListener("click", responsiveMenu);
+iconoNav.addEventListener("click", toggleMenu);
+iconoClose.addEventListener("click", closeMenu);
+linkMenu.forEach(element => element.addEventListener("click", closeMenu));
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 500) closeMenu();
+});
 
-function responsiveMenu() {
+function toggleMenu() {
   menu.classList.toggle("responsive");
 }
 
-iconoClose.addEventListener("click", closeMenu);
-linkMenu.forEach(element => {element.addEventListener("click", closeMenu)});
-window.addEventListener("resize", function () {
-  if (window.innerWidth > 500)
-  closeMenu(); 
-
-});
-
 function closeMenu() {
-  menu.classList.remove("responsive")
+  menu.classList.remove("responsive");
 }
 
-
-window.addEventListener("scroll", function () {
+// Función para manejar el scroll
+window.addEventListener("scroll", () => {
   const contacto = document.querySelector(".contacto");
   const contactoTitulo = document.querySelector(".contacto-titulo");
-  const contactoContenido = [
-    ...document.querySelectorAll(".contacto-contenido"),
-  ];
+  const contactoContenido = document.querySelectorAll(".contacto-contenido");
   const footerLower = document.querySelector(".footer-lower");
+  const scrollPosition = document.documentElement.scrollTop;
+  const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
 
-let scrollPosition = document.documentElement.scrollTop;
-let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
-
-
-  if (scrollPosition >= (totalHeight-10)) {
+  if (scrollPosition >= totalHeight - 10) {
     contacto.style.zIndex = 1;
     footerLower.style.display = "none";
     contactoTitulo.classList.add("titulo-animaciones");
-    contactoContenido.forEach((element) => {
+    contactoContenido.forEach(element => {
       element.classList.add("contenido-animaciones");
     });
   } else {
     contacto.style.zIndex = -1;
     footerLower.style.display = "flex";
     contactoTitulo.classList.remove("titulo-animaciones");
-    contactoContenido.forEach((element) =>
-      element.classList.remove("contenido-animaciones")
-    );
+    contactoContenido.forEach(element => {
+      element.classList.remove("contenido-animaciones");
+    });
   }
 });
 
+// Función para desplazarse al hacer clic en el enlace de contacto
 linkMenu[3].addEventListener('click', contactoMove);
 
+
 function contactoMove() {
-  let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
   window.scrollTo({
     top: totalHeight,
-    behavior: 'auto' 
+    behavior: 'auto'
   });
 }
 
-const sliders = [...document.querySelectorAll(".slider__body")];
+// Funciones para cambiar el slider
+const sliders = document.querySelectorAll(".slider__body");
 const arrowNext = document.querySelector("#next");
 const arrowBefore = document.querySelector("#before");
-
-const currentElement = Number(document.querySelector(".slider__body--show").dataset.id);
+let currentElement = Number(document.querySelector(".slider__body--show").dataset.id);
 
 arrowNext.addEventListener("click", () => changePosition(1));
 arrowBefore.addEventListener("click", () => changePosition(-1));
 
 function changePosition(change) {
-  let value = currentElement;
-  value += change;
+  let value = currentElement + change;
 
-  if (value == -1 || value == sliders.length) {
-    value = (value == -1 ? sliders.length - 1 : 0)
+  if (value === -1 || value === sliders.length) {
+    value = (value === -1 ? sliders.length - 1 : 0);
   }
 
   sliders[currentElement].classList.toggle("slider__body--show");
-  sliders[value].classList.toggle("slider__body--show");  
+  sliders[value].classList.toggle("slider__body--show");
+  currentElement = value;
 }
 
-
+// Funciones para cambiar el tema
 const themeBlue = document.querySelector(".theme--blue");
 const themeGreen = document.querySelector(".theme--green");
-const themeRed= document.querySelector(".theme--red");
-
-themeBlue.addEventListener('click', function() {
-  document.documentElement.style.setProperty('--color-tono', '200');
-});
-
-themeGreen.addEventListener('click', function() {
-  document.documentElement.style.setProperty('--color-tono', '150');
-});
-
-themeRed.addEventListener('click', function() {
-  document.documentElement.style.setProperty('--color-tono', '355');
-});
-
-
-
-
+const themeRed = document.querySelector(".theme--red");
 const btnChangeTheme = document.querySelector(".change-theme");
-btnChangeTheme.addEventListener("click", changeTheme);
 
-function changeTheme() {
-  btnChangeTheme.classList.toggle("uil-sun")
+themeBlue.addEventListener('click', () => changeTheme('--color-tono', '200'));
+themeGreen.addEventListener('click', () => changeTheme('--color-tono', '150'));
+themeRed.addEventListener('click', () => changeTheme('--color-tono', '355'));
 
-  if(btnChangeTheme.classList.contains("uil-sun")){
-    document.documentElement.style.setProperty('--color-fondo', '#000');
-    document.documentElement.style.setProperty('--color-letra', '#fff');
-  }else{
-    document.documentElement.style.setProperty('--color-fondo', '#fff');
-    document.documentElement.style.setProperty('--color-letra', '#000');
-  }
+btnChangeTheme.addEventListener("click", toggleTheme);
 
+function toggleTheme() {
+  btnChangeTheme.classList.toggle("uil-sun");
+  const fondo = btnChangeTheme.classList.contains("uil-sun") ? '#000' : '#fff';
+  const letra = btnChangeTheme.classList.contains("uil-sun") ? '#fff' : '#000';
+
+  document.documentElement.style.setProperty('--color-fondo', fondo);
+  document.documentElement.style.setProperty('--color-letra', letra);
 }
 
+function changeTheme(property, value) {
+  document.documentElement.style.setProperty(property, value);
+}
